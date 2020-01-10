a2902b9947ee0bb1f7bb7fb743e8dde4c8abb910
ys1111 <y0suke111152@gmail.com>
2020-01-10 12:00:17 +0900

    change file type

--- app/App.tsx
+++ app/App.tsx
@@ -48,12 +48,3 @@ export default class App extends React.Component<AppState> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
