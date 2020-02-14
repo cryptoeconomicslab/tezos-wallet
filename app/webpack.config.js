946e28dce30f2801fcd78eb40881a50c766f424d
ys1111 <y0suke111152@gmail.com>
2020-02-15 00:43:32 +0900

    wip webpack.config.externals

---
+++ app/webpack.config.js
@@ -0,0 +1,9 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config')
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv)
+  // Customize the config before returning it.
+  config.externals = ['ethers', 'child_process', 'xmlhttprequest']
+
+  return config
+}
