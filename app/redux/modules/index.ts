77f82cdb345038aac75786ed80ba67b078226edf
ys1111 <y0suke111152@gmail.com>
2020-03-02 19:52:35 +0900

    wip add l1Wallet reducer

--- app/redux/modules/index.ts
+++ app/redux/modules/index.ts
@@ -1,19 +1,18 @@
 import { combineReducers, Reducer, AnyAction } from 'redux'
-import { createNavigationReducer } from 'react-navigation-redux-helpers'
 import appStatus, { State as AppStatusState } from './appStatus'
+import l1Wallet, { State as l1WalletState } from './l1Wallet'
 import address, { State as AddressState } from './address'
-import { NestNavigation } from '../../Navigation'
-
-// const navReducer = createNavigationReducer(NestNavigation)
 
 export interface AppState {
   reducer: {
     appStatus: AppStatusState,
-    address: AddressState
+    address: AddressState,
+    l1Wallet: l1WalletState
   }
 }
 
 export default combineReducers({
   appStatus,
-  address
+  address,
+  l1Wallet
 })
