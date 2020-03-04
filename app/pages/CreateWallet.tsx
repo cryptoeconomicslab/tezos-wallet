52f5acf7107d730c32e6e288f80d3d330bc7ac37
ys1111 <y0suke111152@gmail.com>
2020-03-04 22:34:57 +0900

    add seed

--- app/pages/CreateWallet.tsx
+++ app/pages/CreateWallet.tsx
@@ -26,6 +26,7 @@ class CreateWallet extends Component<Props> {
     try {
       await AsyncStorage.setItem('secretKey', 'edskRpVqFG2FHo11aB9pzbnHBiPBWhNWdwtNyQSfEEhDf5jhFbAtNS41vg9as7LSYZv6rEbtJTwyyEg9cNDdcAkSr9Z7hfvquB')
       await AsyncStorage.setItem('myAddress', 'tz1X3xW1EcS48RQXSdrDTF6xESm933eq251f')
+      await AsyncStorage.setItem('l1Balance', '100')
       await setUserAddress(await AsyncStorage.getItem('myAddress'))
       await Toastr.showToast('success', 'info', 2000)
       await checkClientInitialized()
